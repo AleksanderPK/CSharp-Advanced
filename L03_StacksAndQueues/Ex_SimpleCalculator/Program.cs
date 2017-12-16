dfd3d041d471bff8b8e4b3f09746e494b22d7b5f
Aleksander Kochev <aleksander_k@abv.bg>
2017-12-16 16:47:37 +0200

    SolutionSimpleCalculator

--- L03_StacksAndQueues/Ex_SimpleCalculator/Program.cs
+++ L03_StacksAndQueues/Ex_SimpleCalculator/Program.cs
@@ -10,6 +10,25 @@ namespace Ex_SimpleCalculator
     {
         static void Main(string[] args)
         {
+            var input = "2 + 5 + 10 - 2 - 1";
+            var values = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>(values.Reverse());
+
+            while (stack.Count > 1)
+            {
+                var first = int.Parse(stack.Pop());
+                var op = stack.Pop();
+                var second = int.Parse(stack.Pop());
+
+                switch (op)
+                {
+                    case "+": stack.Push((first + second).ToString()); break;
+                    case "-": stack.Push((first - second).ToString()); break;
+                    default: break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
         }
     }
 }
