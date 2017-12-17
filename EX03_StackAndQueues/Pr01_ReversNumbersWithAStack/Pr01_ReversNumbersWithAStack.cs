3a624aa47fef46acc7b13295a3e9cb9a35a48519
Aleksander Kochev <aleksander_k@abv.bg>
2017-12-17 13:27:45 +0200

    Add_ReverseNumbers

---
+++ EX03_StackAndQueues/Pr01_ReversNumbersWithAStack/Pr01_ReversNumbersWithAStack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX03_StackAndQueues
+{
+    class Pr01_ReversNumbersWithAStack
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var values = input.Split(' ');
+            var stack = new Stack<string>(values);
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
