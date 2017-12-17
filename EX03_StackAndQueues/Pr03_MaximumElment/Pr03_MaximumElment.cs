c44925d6c3a5e13c088af66a7343f6ecfa4d3838
Aleksander Kochev <aleksander_k@abv.bg>
2017-12-17 17:02:15 +0200

    Add_EX03

--- EX03_StackAndQueues/Pr03_MaximumElment/Pr03_MaximumElment.cs
+++ EX03_StackAndQueues/Pr03_MaximumElment/Pr03_MaximumElment.cs
@@ -10,6 +10,30 @@ namespace Pr03_MaximumElment
     {
         static void Main(string[] args)
         {
+            var queriesNum = int.Parse(Console.ReadLine());
+            int[][] inputArr = new int[queriesNum][];
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < queriesNum; i++)
+            {
+                inputArr[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            for (int i = 0; i < queriesNum; i++)
+            {
+                if (inputArr[i][0] == 1)
+                {
+                    stack.Push(inputArr[i][1]);
+                }
+                else if (inputArr[i][0] == 2)
+                {
+                    stack.Pop();
+                }
+                else if (inputArr[i][0] == 3)
+                {
+                    Console.WriteLine(stack.Max());
+                }
+            }
         }
     }
 }
