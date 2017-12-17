d2caa5d538956e422b6d3c1deea214ea2978a3e8
Aleksander Kochev <aleksander_k@abv.bg>
2017-12-17 13:19:56 +0200

    Add_EX02

---
+++ EX03_StackAndQueues/Pr02_BasicStackOperations/Pr02_BasicStackOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr02_BasicStackOperations
+{
+    class Pr02_BasicStackOperations
+    {
+        static void Main(string[] args)
+        {
+            int[] commands = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] elements = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            Stack<int> stack = new Stack<int>(elements);
+
+            for (int i = 0; i < commands[1]; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (stack.Contains(commands[2]))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
---
+++ EX03_StackAndQueues/Pr03_MaximumElment/Pr03_MaximumElment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr03_MaximumElment
+{
+    class Pr03_MaximumElment
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
