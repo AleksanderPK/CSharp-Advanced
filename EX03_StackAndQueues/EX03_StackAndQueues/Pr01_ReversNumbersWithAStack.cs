3cbdbbb66be11bfa52390b381e802f596fdeeeef
Aleksander Kochev <aleksander_k@abv.bg>
2017-12-17 09:20:15 +0200

    Solution_Pr01

--- EX03_StackAndQueues/EX03_StackAndQueues/Pr01_ReversNumbersWithAStack.cs
+++ EX03_StackAndQueues/EX03_StackAndQueues/Pr01_ReversNumbersWithAStack.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace EX03_StackAndQueues
 {
-    class Program
+    class Pr01_ReversNumbersWithAStack
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine();
+            var values = input.Split(' ');
+            var stack = new Stack<string>(values);
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
